60391b5b0a78295c5267f113a57ad40c82616c89
Anthony <anthonysalisbury98@outlook.com>
2025-03-08 18:15:25 +0000

    Setup react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import Button from "@mui/material/Button";
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <>
-      <Button variant="contained">Hello World</Button>
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
   );
 }
 
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function SignUp() {
+  return (
+    <>
+      <h1>Sign Up Page</h1>
+      <Link to="/login">Login</Link>
+    </>
+  );
+}
